50f52fc55aadf5053dd31843c275b741e8a41dce
acottam <adam@gofishcreative.com>
2025-10-27 16:20:48 -0700

    W01E3: C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Radom Generator
+        Random randomGenerator = new Random();
+        
+        // Vars
+        int number = 0;
+        int guess = 0;
+        bool continueLoop = true; 
+
+        while (continueLoop)
+        {
+            // Generate Random Number 1-100
+            number = randomGenerator.Next(1, 101);
+
+            // Welcome Intro
+            Console.WriteLine("I'm thinking of a number between 1 and 100");
+
+            // Allow User to guess
+            do
+            {
+                // Guess
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                // Higher
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                // Lower
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                // Correct
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            } while (guess != number);
+
+            // Do you want to play again?
+            Console.Write("Do you want to play again? (y/n) ");
+            string playAgain = Console.ReadLine();
+
+            // Set to lowercase
+            playAgain = playAgain.Substring(0, 1).ToLower();
+
+            // Quit
+            if (playAgain == "n")
+            {
+                // End outer loop
+                continueLoop = false;
+            }
+        
+        }
+    
     }
 }
