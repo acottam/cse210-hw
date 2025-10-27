4c1cef169574bd37b8e45a7d6c2b80d784070629
acottam <adam@gofishcreative.com>
2025-10-27 09:19:05 -0700

    W01E02: C# Programming Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Get user grade percentage
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        
+        // Cast as float 
+        int grade = int.Parse(gradePercentage);
+
+        string letterGrade = "";
+        string sign = "";
+
+        // Compute Letter Grade
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        
+        // Compute Sign
+        if (letterGrade != "F")
+        {
+            int lastDigit = grade % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+
+        // Output Letter Grade
+        Console.WriteLine($"Your letter grade is: {letterGrade}{sign}");
+
+        // Passed
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. Better luck next time.");
+        }
+        
     }
 }
