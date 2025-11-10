6db2989c8f1f4b5de2491e97dacb8bac576e8ea4
acottam <adam@gofishcreative.com>
2025-11-09 19:47:30 -0800

    W02 Project: Journal Program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int userChoice = -1;
+
+        while (userChoice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            
+            string userResponse = Console.ReadLine();
+            userChoice = int.Parse(userResponse);
+
+            if (userChoice == 1)
+            {
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = promptGenerator.GetRandomPrompt();
+                
+                Console.WriteLine(entry._promptText);
+                Console.Write("> ");
+                entry._entryText = Console.ReadLine();
+                
+                theJournal.AddEntry(entry);
+            }
+            else if (userChoice == 2)
+            {
+                theJournal.DisplayAll();
+            }
+            else if (userChoice == 3)
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.LoadFromFile(filename);
+            }
+            else if (userChoice == 4)
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                theJournal.SaveToFile(filename);
+            }
+        }
     }
 }
